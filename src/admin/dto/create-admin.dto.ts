7fc7a9f9581b8e8a59a530d3f53290a711d82249
GOKULSCSE <gokul03903@gmail.com>
2025-11-05 12:32:20 +0530

    admin bashboard

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -8,18 +8,13 @@ import {
 import { UserRole } from '@prisma/client';
 
 export class CreateAdminDto {
+  // User account fields
   @IsEmail()
   email: string;
 
   @IsString()
   @MinLength(8)
   password: string;
-
-  @IsString()
-  firstName: string;
-
-  @IsString()
-  lastName: string;
 
   @IsOptional()
   @IsString()
@@ -30,11 +25,62 @@ export class CreateAdminDto {
   })
   role: UserRole;
 
+  // Admin profile fields
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
   @IsOptional()
   @IsString()
   department?: string;
 
   @IsOptional()
   @IsString()
-  position?: string;
+  designation?: string;
+
+  @IsOptional()
+  @IsString()
+  bio?: string;
+
+  @IsOptional()
+  @IsString()
+  profileImage?: string;
+
+  // Address fields
+  @IsOptional()
+  @IsString()
+  country?: string;
+
+  @IsOptional()
+  @IsString()
+  state?: string;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  streetAddress?: string;
+
+  // Social media fields
+  @IsOptional()
+  @IsString()
+  linkedinUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  facebookUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  twitterUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  instagramUrl?: string;
 }
